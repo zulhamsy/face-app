b21854ea7157d5a5eb4717eb8518a80878276713
Zulham Syafrawi <official.zulham@gmail.com>
2021-08-24 14:35:22 +0700

    fixing fetching in singleView Mode

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,8 @@ import { auth, challengesDB } from '../firebase'
 const store = {
   state() {
     return {
-      challenges: []
+      challenges: [],
+      challenge: {}
     }
   },
   mutations: {
@@ -14,6 +15,20 @@ const store = {
     },
     removeAll(state) {
       state.challenges = []
+    },
+    updateFailed(state, { id, payload }) {
+      const index = state.challenges.findIndex(
+        (challenge) => challenge.id == id
+      )
+      state.challenges[index].failed = [payload]
+    },
+    updateSingle(state, payload) {
+      state.challenge = payload
+    }
+  },
+  getters: {
+    challengeIndex: (state) => (id) => {
+      return state.challenges.findIndex((challenge) => challenge.id == id)
     }
   },
   actions: {
@@ -34,7 +49,7 @@ const store = {
         desc: description,
         startDate: today,
         endDate: end,
-        failed: null
+        failed: []
       }
       const docRef = await challengesDB.add(payload)
       commit('add', {
@@ -60,12 +75,41 @@ const store = {
         })
       } // else ketika datanya kosong jadi return message buat dashboard
     },
-    async fetchSingleChallenge({ commit }, id) {
-      const snapshot = await challengesDB.doc(id).get()
-      if (snapshot.exists) {
-        console.log(snapshot.data())
-        commit('add', snapshot.data())
-      } // else ketika datanya kosong, jadi tampilkan 404
+    async fetchHistory({ commit }, id) {
+      const query = challengesDB
+        .doc(id)
+        .collection('history')
+        .where('type', '==', 'failed')
+      const snapshot = await query.get()
+      if (!snapshot.empty) {
+        snapshot.forEach((doc) => {
+          commit('updateFailed', { id, doc })
+        })
+      }
+    },
+    async fetchSingleChallenge({ commit, state, getters }, id) {
+      // cek apakah id di state.challenge == id | kalo ada return true
+      if (state.challenge && state.challenge.id == id) {
+        return true
+        // else if, cek apakah di state.challenges ada object.id == id | kalo ada set challenge
+      } else if (getters.challengeIndex(id) != -1) {
+        state.challenge = state.challenges[getters.challengeIndex(id)]
+      } else {
+        // else, fetch single data dan store di state.challenge
+        const doc = await challengesDB.doc(id).get()
+        if (doc.exists) {
+          commit('updateSingle', {
+            id: doc.id,
+            active: doc.data().active,
+            goalType: doc.data().goalType,
+            title: doc.data().title,
+            desc: doc.data().desc,
+            startDate: doc.data().startDate.toDate(),
+            endDate: doc.data().endDate.toDate(),
+            failed: doc.data().failed
+          })
+        }
+      }
     }
   }
 }
--- src/views/ViewChallenge.vue
+++ src/views/ViewChallenge.vue
@@ -2,35 +2,37 @@
   <!-- Navbar -->
   <the-navbar />
   <!-- Content -->
-  <div class="container mx-auto px-4 pt-16">
+  <div
+    v-if="Object.keys(challenge).length"
+    class="container mx-auto px-4 pt-16"
+  >
     <main
-      v-if="selectedChallenge"
       class="space-y-8"
     >
       <!-- Short Desc -->
       <div>
         <base-input-label>Short Description</base-input-label>
-        <p>{{ selectedChallenge.title }}</p>
+        <p>{{ challenge.title }}</p>
       </div>
       <!-- Description -->
       <div>
         <base-input-label>Description</base-input-label>
-        <p>{{ selectedChallenge.desc }}</p>
+        <p>{{ challenge.desc }}</p>
       </div>
       <!-- Starting Date -->
       <div>
         <base-input-label>Starting Date</base-input-label>
-        <p>{{ dayName(selectedChallenge.startDate) }}</p>
+        <p>{{ dayName(challenge.startDate) }}</p>
       </div>
       <!-- Ending Date-->
       <div>
         <base-input-label>Ending Date</base-input-label>
-        <p>{{ dayName(selectedChallenge.endDate) }}</p>
+        <p>{{ dayName(challenge.endDate) }}</p>
       </div>
       <!-- Failed Times -->
       <div>
         <base-input-label>Failed</base-input-label>
-        <p>{{ selectedChallenge.failed ? selectedChallenge.failed : 0 }} times</p>
+        <p>{{ challenge.failed.length ? challenge.failed.length : 0 }} times</p>
       </div>
       <!-- CTA -->
       <div class="space-y-3">
@@ -53,24 +55,15 @@
 
 <script>
 import { mapState, mapActions } from 'vuex'
+// import { challengesDB } from '../firebase'
 export default {
   name: 'ViewChallenge',
   computed: {
-    ...mapState(['challenges']),
-    selectedChallenge() {
-      if (this.challenges.length) {
-        return this.challenges.filter(
-          (challenge) => challenge.id == this.$route.params.id
-        )[0]
-      } else {
-        return false
-      }
-    }
+    ...mapState(['challenge'])
   },
   created() {
-    if (this.challenges.length == 0) {
-      this.fetchChallenge()
-    }
+    this.fetchSingleChallenge(this.$route.params.id)
+    // this.fetchHistory(this.$route.params.id)
   },
   methods: {
     dayName(date) {
@@ -101,7 +94,7 @@ export default {
         month[date.getMonth()]
       } ${date.getFullYear()}`
     },
-    ...mapActions(['fetchChallenge'])
+    ...mapActions(['fetchHistory', 'fetchSingleChallenge'])
   }
 }
 </script>
